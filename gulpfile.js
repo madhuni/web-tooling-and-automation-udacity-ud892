3079bd5d896932420800477816b013a89a2dc1b2
Kanishka Madhuni <madhuni@Kanishkas-MacBook-Air.local>
2017-08-21 13:48:16 +0530

    chore: Setup the gulp for the project

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp'); // as this is a node JS script we need 'gulp' as dependency
+
+gulp.task('default', function() {
+  // place code for your default task here
+
+  console.log("Hey welcome to Gulp learning");
+});
+
+console.log(gulp);
